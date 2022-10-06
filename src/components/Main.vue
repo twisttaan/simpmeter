3cac9f446f37c6265b92b0a57383a53817690470
adrifcastr <adrifcastr@gmail.com>
2022-10-06 21:11:41 +0200

    basic prototype

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	import { getGlobalThis } from '@vue/shared';
+import { toggleButton, handleFileUpload } from '../scripts/history';
+let app = getGlobalThis();
+</script>
+
+<template>
+  <div class="card">
+	<div class="card-body">
+	  <div class="card-title">
+		<h2>Simp-O-Meter</h2>
+	  </div>
+		<div class="card-text">
+			<p>Upload your watch-history.json file to see your watch history.</p>
+			<input id="file" type="file" accept=".json" @change="toggleButton($event, app)" />
+			<button id="analyze" @click="handleFileUpload($event, app)">Analyze!</button>
+		</div>
+		</div>
+	</div>
+</template>
